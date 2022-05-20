966dce54ab12e8ee8dee424c59e0cc51d83a6dcc
LIYIKANG <45025866+LIYIKANG@users.noreply.github.com>
2022-05-20 11:34:26 +0900

    Create assume.h

---
+++ assume.h
@@ -0,0 +1,18 @@
+/* ASSUME() works same as standard-function assert().
+   This function ignores NODEBUG macro, unlikely assert(). */
+
+#pragma once
+#ifndef INCLUDE_ASSUME_H_
+#define INCLUDE_ASSUME_H_
+
+#if !defined(ASSUME)
+  #include <cassert>
+  #define ASSUME(expr) \
+    ((expr) ? ((void)(0)) : __assert_fail(#expr, __FILE__, __LINE__, __ASSERT_FUNCTION))
+#else
+  #error already defined: ASSUME()
+#endif // !defined(ASSUME)
+
+#endif // INCLUDE_ASSUME_H_
+
+// end of file
